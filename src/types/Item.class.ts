ee91c592308139074bd0633b11e2bf565b4ed8a3
Alex <alex@precision-analytics.ca>
2021-06-20 21:36:45 -0400

    Add Player and Item classes

---
+++ src/types/Item.class.ts
@@ -0,0 +1,7 @@
+export class Item {
+  readonly name: string;
+
+  constructor(name: string) {
+      this.name = name;
+  }
+};
---
+++ src/types/Player.class.ts
@@ -0,0 +1,17 @@
+import type { Item } from "./Item.class";
+
+export class Player {
+  readonly inventory: {[key:string]: Item}
+
+  constructor() {
+      this.inventory = {};
+  }
+
+  add_item(item: Item) {
+      this.inventory[item.name] = item;
+  }
+
+  has_item(item_name: string) {
+      return this.inventory[item_name] == undefined ? false : true;
+  }
+}
