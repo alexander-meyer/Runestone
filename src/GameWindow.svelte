6468837381e60b6c00f2fc78256e051028ce3eac
Alex <alex@precision-analytics.ca>
2021-07-30 08:28:37 -0400

    Remove game logic from `App.svelte`
    This commit ensures that all of the game logic is contained in the
    `GameWindow` component. Instead of having portions of the logic
    contained in the `App` component, we ensure the functionality is
    decoupled and that `GameWindow` is a standalone component.

--- src/GameWindow.svelte
+++ src/GameWindow.svelte
@@ -5,8 +5,17 @@
 
   let game: Game;
   let user_input: string;
+  let show_start = false;
 
-  const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Duis ut diam quam nulla porttitor massa id. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Duis ut diam quam nulla porttitor massa id. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Duis ut diam quam nulla porttitor massa id.";
+  const infoScreen = `This is a petite adventure where you traverse different areas to discover secrets and solve puzzles. Some of the basic commands are:
+    <ul>
+    <li><b>go</b> _____ : move between rooms</li>
+    <li><b>examine</b> : investigate a particular object</li>
+    <li><b>bag</b> : check the contents of your inventory</li>
+    <li><b>where am I</b> : information about your current location</li>
+    </ul>
+    Areas may also contain hidden triggers that respond to additional prompts. Type <i>help</i> at any time to bring up this info. <br/><br/>
+  `
 
   onMount(function() {
     game = new Game("meadow", world)
@@ -20,15 +29,69 @@
 
 </script>
 
-<div class="game-screen">
-  <div id="game-text"/>
-  <div class="input-container">
-    <span class="input-indicator">></span>
-    <input id="user-input" placeholder="What do you do?" on:keypress={handle_key_press} bind:value={user_input}/>
+<main>
+  <div class="container">
+    {#if show_start}
+      <h1 class="title">Runestone</h1>
+      <div class="start-screen">
+        <span>{@html infoScreen}</span>
+        <button class="start-button" on:click= { function() { show_start = false; } }>Start</button>
+      </div>
+    {:else}
+      <div class="game-screen">
+        <div id="game-text"/>
+        <div class="input-container">
+          <span class="input-indicator">></span>
+          <input id="user-input" placeholder="What do you do?" on:keypress={handle_key_press} bind:value={user_input}/>
+        </div>
+      </div>
+    {/if}
   </div>
-</div>
+</main>
+
 
 <style>
+  main {
+    color: rgb(70, 54, 30);
+    height: 100%
+  }
+
+  .title {
+    font: bold 4em 'Lora', serif;
+    text-align: center;
+  }
+
+  .container {
+    margin: 2em auto;
+    width: 70%;
+    height: 80%;
+  }
+
+  .start-screen {
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lora', serif;
+  }
+
+  .start-screen > button {
+    background-color: rgb(70, 54, 30);
+    border: none;
+    border-radius: 4px;
+    color: rgb(238, 238, 238);
+    cursor: pointer;
+    font-size: 1.2em;
+    font-family: 'Lora', serif;
+    height: 2.5em;
+    margin: auto;
+    opacity: 0.8;
+    transition-duration: 0.4s;
+    width: 5em;
+  }
+
+  .start-button:hover {
+    opacity: 1;
+  }
+
   .game-screen {
     height: 90%;
     font-family: "Lora", serif;
